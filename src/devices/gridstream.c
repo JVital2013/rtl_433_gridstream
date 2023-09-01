5c6452672d22643580b27ec67e5c6011471480c5
krvmk <47903504+krvmk@users.noreply.github.com>
2023-09-01 16:12:14 -0500

    Optimize decoding addresses

--- src/devices/gridstream.c
+++ src/devices/gridstream.c
@@ -47,6 +47,8 @@ Data layouts:
 
 #include "decoder.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 static uint16_t known_crc_init[] = {
 0xe623, 0x5fd6, 0xD553, 0x45F8, 0x62C1, 0x23D1, 0x2C22, 0x142A, };
@@ -60,11 +62,13 @@ static int gridstream_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     uint16_t crc;
     uint16_t crc_count = 0;
     bool crc_ok = false;
-    uint64_t wanaddress, destaddress;
-    uint32_t srcaddress;
-    char srcaddress_str[9+1],wanaddress_str[13+1],destaddress_str[13+1];
+    char destwanaddress_str[13];
+    char srcwanaddress_str[13];
+    char srcaddress_str[9];
+    char destaddress_str[9];
     uint32_t uptime;
-    uint32_t clock;
+    time_t clock;
+    char clock_str[80];
     unsigned offset = bitbuffer_search(bitbuffer, 0, 0, preamble, 24) + 16;
     /* TODO: Special handling for V4 and V5 syncwords as V5 is not compatible with extract_bytes_uart() */
     int decoded_len = extract_bytes_uart(bitbuffer->bb[0],offset,bitbuffer->bits_per_row[0]-offset,b);
@@ -89,22 +93,21 @@ static int gridstream_decode(r_device *decoder, bitbuffer_t *bitbuffer)
                         }
                         while ( crc_count < (sizeof(known_crc_init)/2) && crc_ok == false );
                         if (!crc_ok) {
-                            
+
                             return DECODE_FAIL_MIC;
                         }
-                        destaddress = ((uint64_t)b[7] << 40) | ((uint64_t)b[8] << 32) | (b[9] << 24) | (b[10] << 16) | (b[11] << 8) | b[12];
-                        wanaddress = ((uint64_t)b[13] << 40) | ((uint64_t)b[14] << 32) | (b[15] << 24) | (b[16] << 16) | (b[17] << 8) | b[18];
+
+                        sprintf(destwanaddress_str, "%02x%02x%02x%02x%02x%02x", b[7], b[8], b[9], b[10], b[11], b[12]);
+                        sprintf(srcwanaddress_str, "%02x%02x%02x%02x%02x%02x", b[13], b[14], b[15], b[16], b[17], b[18]);
+                        sprintf(srcaddress_str, "%02x%02x%02x%02x", b[26], b[27], b[28], b[29]);
                         uptime = ((uint32_t)b[20] << 24) | (b[21] << 16) | (b[22] << 8) | b[23];
-                        srcaddress = ((uint32_t)b[26] << 24) | (b[27] << 16) | (b[28] << 8) | b[29];
-                        sprintf(destaddress_str, "%12lx", destaddress);
-                        sprintf(wanaddress_str, "%12lx", wanaddress);
-                        sprintf(srcaddress_str,"%08x", srcaddress);
+
                         /* clang-format off */
                         data = data_make(
                                 "model",      "", DATA_STRING, "LandisGyr GridStream",
                                 "id",         "", DATA_STRING,    srcaddress_str,
-                                "wanaddress", "", DATA_STRING,    wanaddress_str,
-                                "destaddress", "", DATA_STRING,   destaddress_str,
+                                "wanaddress", "", DATA_STRING,    srcwanaddress_str,
+                                "destaddress", "", DATA_STRING,   destwanaddress_str,
                                 "uptime",     "", DATA_INT,    uptime,
                                 "mic",        "", DATA_STRING, "CRC", // CRC, CHECKSUM, or PARITY
                                 NULL);
@@ -157,24 +160,22 @@ static int gridstream_decode(r_device *decoder, bitbuffer_t *bitbuffer)
                         if (!crc_ok) {
                             return DECODE_FAIL_MIC;
                         }
-                        destaddress = ((uint32_t)b[7] << 24) | (b[8] << 16) | (b[9] << 8) | b[10];
-                        srcaddress = ((uint32_t)b[11] << 24) | (b[12] << 16) | (b[13] << 8) | b[14];
-                        sprintf(destaddress_str,"%08lx", destaddress);
-                        sprintf(srcaddress_str,"%08x", srcaddress);
+                        sprintf(destaddress_str, "%02x%02x%02x%02x", b[7], b[8], b[9], b[10]);
+                        sprintf(srcaddress_str, "%02x%02x%02x%02x", b[26], b[27], b[28], b[29]);
                         if (stream_len == 0x47) {
                             clock = ((uint32_t)b[16] << 24) | (b[17] << 16) | (b[18] << 8) | b[19];
                             uptime = ((uint32_t)b[24] << 24) | (b[25] << 16) | (b[26] << 8) | b[27];
-                            wanaddress = ((uint64_t)b[32] << 40) | ((uint64_t)b[33] << 32) | ((uint32_t)b[34] << 24) | (b[35] << 16) | (b[36] << 8) | b[37];
-                            sprintf(wanaddress_str, "%12lx", wanaddress);
+                            sprintf(srcwanaddress_str, "%02x%02x%02x%02x%02x%02x", b[32], b[33], b[34], b[35], b[36], b[37]);
+                            strftime(clock_str,sizeof(clock_str), "%a %Y-%m-%d %H:%M:%S %Z", localtime(&clock));
 
                             /* clang-format off */
                             data = data_make(
                                     "model",        "", DATA_STRING, "LandisGyr GridStream",
                                     "id",           "", DATA_STRING, srcaddress_str,
                                     "destaddress",  "", DATA_STRING, destaddress_str,
-                                    "timestamp",    "", DATA_STRING, clock,
+                                    "timestamp",    "", DATA_STRING, clock_str,
                                     "uptime",       "", DATA_INT,    uptime,
-                                    "wanaddress",   "", DATA_STRING, wanaddress_str,
+                                    "wanaddress",   "", DATA_STRING, srcwanaddress_str,
                                     "mic",          "", DATA_STRING, "CRC", // CRC, CHECKSUM, or PARITY
                                     NULL);
                             /* clang-format on */
@@ -218,7 +219,7 @@ static char const *const output_fields[] = {
         "uptime",
         "srclocation",
         "destlocation",
-        "time",
+        "timestamp",
         "mic",
         NULL,
 };
